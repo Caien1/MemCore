d97d204e168c04e66b981f1c19fb305d5c37f9d5
Caien1 <pulwama01@gmail.com>
2026-03-30 14:53:43 +0530

    simple logging function

---
+++ logger.cpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+
+#include <cmath>
+#include <ctime>
+
+enum Severity { LOG, WARNING, ERROR };
+void logger(Severity level, std::string msg) {
+
+  // TODO check if this method of making time stamps is accurate and append logs
+  // to a file
+  time_t now = time(nullptr);
+  struct tm *tm_info = localtime(&now);
+  char buffer[20];
+  std::strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", tm_info);
+
+  std::string SeverityMap[3] = {"[LOG]", "[WARNING]", "ERROR"};
+  std::string tab[3] = {"[+]", "[*]", "[=]"};
+
+  std::cout << tab[level] << SeverityMap[level] << " Timestamp: " << buffer
+            << " " << msg << std::endl;
+}
