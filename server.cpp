c61178cf4c1ecad1f7e0ad6835bff42d54418a5b
Caien1 <pulwama01@gmail.com>
2026-04-02 19:48:10 +0530

    changed objects to simple structs

--- server.cpp
+++ server.cpp
@@ -1,66 +1,113 @@
 #include "./logger.cpp"
 #include <arpa/inet.h>
+#include <cstddef>
+#include <cstdint>
 #include <format>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
-typedef struct RedisServer {
+typedef struct {
+  size_t capacity;
+  size_t size;
+  uint8_t *buffer;
+} Arena;
+
+typedef struct {
   int port;
   int serverSocket;
-  bool running; // TODO change to atomic bool later for concurrency issues
-
-  RedisServer(int port) : port(port), serverSocket(0), running(false) {}
-
-  void log() {
-    std::string runningStatus =
-        this->running ? std::format("Runnin on port:{} ", this->port)
-                      : "Not Running";
-
-    logger(Severity::INFO, runningStatus);
-  }
-
-  void socketInit() {
-    int serverSocketFd = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocketFd < 0) {
-      logger(Severity::ERROR, "Couldn't create Socket FD");
-      exit(1);
-    } else {
-      logger(Severity::LOG, "Initialised Socket FD");
-    }
-
-    struct sockaddr_in serverAddr;
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(this->port);
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
-
-    int bindSucess = bind(serverSocketFd, (struct sockaddr *)&serverAddr,
-                          sizeof(struct sockaddr_in));
-    if (bindSucess == 0) {
-      logger(Severity::LOG, "Binded Address and port with socket");
-    } else {
-      logger(Severity::ERROR, "Could Bind Adress and port");
-      exit(1);
-    }
-
-    int listenerSucess =
-        listen(serverSocketFd, 5); // TODO ceck out this command in detail and
-                                   // max simultaneous connections
-    if (listenerSucess == 0) {
-      logger(Severity::LOG, "Listening");
-    } else {
-      logger(Severity::ERROR, "Listner failed to Initialise");
-      exit(1);
-    }
-    // TODO check serveropt() function
-    struct sockaddr_in clientAddr;
-    socklen_t clientSize = sizeof(clientAddr);
-
-    /*-------------unchecked------- */
-    int optval = 1;
-    setsockopt(serverSocketFd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int));
-  }
-
-  void socketClose() { close(this->serverSocket); }
+  bool running; // TODO conccurrency thingy
 
 } RServer;
+
+void log(RServer *server) {
+  std::string runningStatus =
+      server->running ? std::format("Runnin on port:{} ", server->port)
+                      : "Not Running";
+
+  logger(Severity::INFO, runningStatus);
+}
+
+void socketInit(RServer *server) {
+  server->serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+  if (server->serverSocket < 0) {
+    logger(Severity::ERROR, "Couldn't create Socket FD");
+    exit(1);
+  } else {
+    logger(Severity::LOG, "Initialised Socket FD");
+  }
+
+  struct sockaddr_in serverAddr;
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_port = htons(server->port);
+  serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+  int bindSucess = bind(server->serverSocket, (struct sockaddr *)&serverAddr,
+                        sizeof(struct sockaddr_in));
+  if (bindSucess == 0) {
+    logger(Severity::LOG, "Binded Address and port with socket");
+  } else {
+    logger(Severity::ERROR, "Could Bind Adress and port");
+    exit(1);
+  }
+
+  int listenerSucess =
+      listen(server->serverSocket, 5); // TODO ceck out this command in detail
+                                       // and max simultaneous connections
+  if (listenerSucess == 0) {
+    logger(Severity::LOG, "Listening");
+  } else {
+    logger(Severity::ERROR, "Listner failed to Initialise");
+    exit(1);
+  }
+
+  /*-------------unchecked------- */
+  int optval = 1;
+  setsockopt(server->serverSocket, SOL_SOCKET, SO_REUSEADDR, &optval,
+             sizeof(int));
+}
+
+void acceptLoop(RServer *server) {
+  struct sockaddr_in clientAddr;
+  socklen_t clientSize = sizeof(clientAddr);
+  char buffer[1024];
+  char buff2[] = "pong";
+  for (;;) {
+    int clientFd = accept(server->serverSocket, (struct sockaddr *)&clientAddr,
+                          &clientSize);
+    if (clientFd < 0) {
+      logger(Severity::ERROR, "Error in accept");
+      continue;
+    }
+    std::string msg =
+        std::format("New connections from {}:{}",
+                    inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
+    logger(Severity::INFO, msg);
+
+    int recvLength = 0;
+
+    while (true) {
+      recvLength = recv(clientFd, buffer, sizeof(buffer), 0);
+      if (recvLength < 0) {
+        logger(Severity::ERROR, std::format("Recv Failed for {}:{}",
+                                            inet_ntoa(clientAddr.sin_addr),
+                                            ntohs(clientAddr.sin_port)));
+        ;
+        break;
+      } else if (recvLength == 0) {
+        logger(Severity::INFO, std::format("Client: {} : {} disconnected ",
+                                           inet_ntoa(clientAddr.sin_addr),
+                                           ntohs(clientAddr.sin_port)));
+        break;
+      } else {
+        // TODO Fix logical bugs here
+        //
+
+        buffer[recvLength] = '\0';
+        logger(Severity::LOG, buffer);
+        send(clientFd, buff2, sizeof(buff2), 0);
+      }
+    }
+  }
+}
+void socketClose(RServer *server) { close(server->serverSocket); }
